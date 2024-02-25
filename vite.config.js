f150dd6a8259e8c6bd44aff2902e7223320bd513
artur282 <62455365+artur282@users.noreply.github.com>
2024-02-25 04:01:39 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
-import { defineConfig } from "vite";
+/juego-electiva-5/import { defineConfig } from "vite";
 
 
 export default defineConfig({
-    base: '/practica-vite-blackjack/'
+    base: '/juego-electiva-5/'
 })
